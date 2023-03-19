056d2efce6594efb2e86c7e7d3c5586d4a0d0b32
youssef <youssefelkhadiri123@gmail.com>
2023-03-19 11:22:46 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 /**
-*main - entry poit
-*return: always 0 (success)
+ * main - Entry point 
+ * Description: A C program that prints with put function
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
