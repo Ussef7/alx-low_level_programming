3c2b219717beca6b8d1fa8a1eb3525b3e4c33185
youssef <youssefelkhadiri123@gmail.com>
2023-05-04 06:31:39 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - Used to print the binary equivalent of the(decimal number).
+ * @n: The number that is printed in binary.
+ */
+void print_binary(unsigned long int n)
+{
+	int t, count = 0;
+	unsigned long int curt;
+
+	for (t = 63; t >= 0; t--)
+	{
+		curt = n >> t;
+
+		if (curt & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
