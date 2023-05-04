cf9b80e8d84337e00690b93b9883559cf6e2bde1
youssef <youssefelkhadiri123@gmail.com>
2023-05-04 07:24:54 -0700

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - Used to check if the machine is (SMALL) or (BIG) endian.
+ * Return: 0 for (BIG), 1 for (SMALL)
+ */
+int get_endianness(void)
+{
+	unsigned int k = 1;
+	char *g = (char *) &k;
+
+	return (*g);
+}
