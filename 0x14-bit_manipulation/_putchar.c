23882fc11dba3e3a0ca8ff6cd62f4ca018e3474e
youssef <youssefelkhadiri123@gmail.com>
2023-07-06 14:15:03 -0700

    p

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - Used to be writing the character (c), to the stdout.
+ * @c: A Character to be printed.
+ *
+ * Return: 'Success' (1)
+ * On error,Returned (-1), error is appropriately seted.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
